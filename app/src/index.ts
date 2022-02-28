f4cc2137f6c3c90fd1a3f6be3334e8eab92038da
Michiel Hegemans <hegemansmichiel@gmail.com>
2022-02-28 11:20:05 +0100

    Fix some linter errors.

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,9 @@
 function start(argv: string[]) {
     console.log('Hello world!');
 
-    const a: any = { message: 'Goodbye world!' };
+    const a: Record<string, string> = { message: 'Goodbye world!' };
 
-    console.log(a.message); 
+    console.log(a.message);
 }
 
 start(process.argv);
