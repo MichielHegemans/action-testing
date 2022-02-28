5850b37609ad59c4ea44b193dce7fa37dcb78a53
Michiel Hegemans <hegemansmichiel@gmail.com>
2022-02-28 11:23:17 +0100

    Add useless code

--- app/src/index.ts
+++ app/src/index.ts
@@ -4,6 +4,8 @@ function start(argv: string[]) {
     const a: Record<string, string> = { message: 'Goodbye world!' };
 
     console.log(a.message);
+
+    const b: string = 'Hello universe';
 }
 
 start(process.argv);
